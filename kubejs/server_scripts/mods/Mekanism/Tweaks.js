f1c32b1f7d02d4f005c4584539a93e40bcf273cb
RTN <nightearl015@gmail.com>
2025-02-28 21:53:49 +0800

    updated

--- kubejs/server_scripts/mods/Mekanism/Tweaks.js
+++ kubejs/server_scripts/mods/Mekanism/Tweaks.js
@@ -5,4 +5,10 @@ ServerEvents.tags('item', event => {
     event.remove('forge:salt', 'mekanism:salt')
     event.add('forge:industrial_salt', 'mekanism:salt')
     event.add('mekanism:industrial_salt', 'mekanism:salt')
+
+    event.replaceInput(
+        { mod: 'mekanism' }, 
+        '#forge:salt',            
+        '#mekanism:industrial_salt' 
+    )
 })
