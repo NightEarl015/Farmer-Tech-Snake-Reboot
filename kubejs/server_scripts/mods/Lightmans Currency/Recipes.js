c154f309002ce3c25bdead886d791be029106591
RTN <nightearl015@gmail.com>
2025-03-05 16:53:04 +0800

    updated

--- kubejs/server_scripts/mods/Lightmans Currency/Recipes.js
+++ kubejs/server_scripts/mods/Lightmans Currency/Recipes.js
@@ -6,43 +6,34 @@ ServerEvents.recipes(event => {
         'copper', 'iron', 'gold', 'emerald', 'diamond', 'netherite', 'nether_star'
     ]; // Defining types
 
-    const traders = [
-        'item_trader', 'fluid_trader', 'energy_trader'
-    ]; // Defining traders
-
     const cointypes = [
          'coin', 'coinpile', 'coinblock'
     ]; // Defining cointypes
 
     const types = [
         'block', 'coin', 'pile'
-    ]; // Defining types
+   ]; // Defining types
 
     levels.forEach(level => {
         cointypes.forEach(cointype => {
             types.forEach(type => {
-                traders.forEach(trader => {
-                    event.remove({output: `lightmanscurrency:wallet_${level}`}),
-                    event.remove({output: 'lctech:battery'}),
-                    event.remove({output: 'lctech:battery_large'})
-                    event.remove({type: 'lightmanscurrency:coin_mint'})
-                    event.remove({id: 'lightmanscurrency:coin_mint'})
-                    event.remove({id: `lightmanscurrency:${cointype}_${level}_from_${type}`})
-                    event.remove({id: `lightmanscurrency:wallet/upgrade_wallet_${level}_to_wallet_nether_star`})
-
-                    event.replaceInput(
-                        { id: `lightmanscurrency:${trader}_interface`}, 
-                        'minecraft:iron_ingot',            
-                        'createutilities:void_steel_ingot'         
-                    )
-
-                    event.replaceInput(
-                        {}, 
-                        'lctech:battery',            
-                        'createaddition:modular_accumulator'         
-                    )
-                });
+                event.remove({output: `lightmanscurrency:wallet_${level}`}),
+                event.remove({output: 'lctech:battery'}),
+                event.remove({output: 'lctech:battery_large'})
+                event.remove({type: 'lightmanscurrency:coin_mint'})
+                event.remove({id: 'lightmanscurrency:coin_mint'})
+                event.remove({id: `lightmanscurrency:${cointype}_${level}_from_${type}`})
+                event.remove({id: `lightmanscurrency:wallet/upgrade_wallet_${level}_to_wallet_nether_star`})
             });
         });            
     });
+
+    /*
+    e.replaceInput([
+        {id: "lightmanscurrency:item_trader_interface"},
+        {id: "lctech:fluid_trader_interface"},
+        {id: "lctech:energy_trader_interface"}
+    ], "minecraft:iron_ingot", "createutilities:void_steel_ingot")
+    e.replaceInput({}, "lctech:battery", "createaddition:modular_accumulator")
+    */
 })
